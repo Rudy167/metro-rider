5b9d6845ff2b7705c8fa468ece7aeb65ffc140eb
Amit Dhotay <amitdhotay121@gmail.com>
2024-11-25 17:57:28 +0530

    initial commit

---
+++ src/main/java/com/metrorider/MetroRiderApplication.java
@@ -0,0 +1,13 @@
+package com.metrorider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroRiderApplication.class, args);
+	}
+
+}
